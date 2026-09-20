facfc28fb0f2f0d368a41d43924af7b0d19e935c
shashi0396 <shashikirandc1315@gmail.com>
2026-09-20 18:50:37 +0530

    updated node group module

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source             = "terraform-aws-modules/eks/aws"
   version            = "21.24.2"
-  region            = var.aws_region
+  region             = var.aws_region
   name               = var.cluster_name
   kubernetes_version = var.cluster_version
   # ==========================================================
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-aws_region = "ap-south-1"
+aws_region = "us-east-1"
 
 project_name = "terraform-eks"
 
