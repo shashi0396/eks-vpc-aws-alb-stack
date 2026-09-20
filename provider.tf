cf4ec5b35a6d9f04145d8ae86a0722d29b06f947
shashi0396 <shashikirandc1315@gmail.com>
2026-09-20 18:47:44 +0530

    updated node group module

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ data "aws_availability_zones" "available" {
 
 data "aws_caller_identity" "current" {}
 
-data "aws_partition" "current" {}
+# data "aws_partition" "current" {}
 
 # ============================================================
 # KUBERNETES PROVIDER
